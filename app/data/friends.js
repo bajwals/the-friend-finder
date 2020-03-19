800ab6402934f43af97c231b8ccb75a06628a971
Lakshdeep Bajwa <bajwals@live.com>
2020-03-18 17:36:44 -0700

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+    name: "Abby Smith",
+    photo: "https://pbs.twimg.com/profile_images/1055733752968577026/qbSFLs-t_400x400.jpg",
+    scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+    ]
+    },
+    {
+    name: "Bella Smith",
+    photo: "https://pbs.twimg.com/profile_images/1224331597253472256/Ak67eGDJ_400x400.jpg",
+    scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+    ]
+    },
+    {
+    name: "Cassidy Smith",
+    photo: "https://pbs.twimg.com/profile_images/1233266821433704449/RWX40Ymv_400x400.jpg",
+    scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+    ]
+    },
+    {
+    name: "Dorothy Smith",
+    photo: "https://pbs.twimg.com/profile_images/1226169967915061249/Q8LZ4UXW_400x400.jpg",
+    scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+    ]
+    },
+    {
+    name: "Ella Smith",
+    photo: "https://pbs.twimg.com/profile_images/1234454294230732801/WJ_7Mwex_400x400.jpg",
+    scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+    ]
+    },
+    {
+    name: "Felicia Smith",
+    photo: "https://pbs.twimg.com/profile_images/1236800569819291650/EHkWiGYq_400x400.jpg",
+    scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+    ]
+    }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
